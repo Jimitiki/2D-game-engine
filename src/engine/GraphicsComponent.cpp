131c6291516d8ea841f4cea1a0cc5f5cd149e981
Chris Johnson <cwjohnson42@gmail.com>
2018-09-07 23:24:25 -0700

    Reduced number of memory allocations

--- src/engine/GraphicsComponent.cpp
+++ src/engine/GraphicsComponent.cpp
@@ -4,23 +4,9 @@
 
 #include "AssetManager.hpp"
 
-GraphicsComponent::GraphicsComponent(std::string* image_name, int src_x, int src_y, int src_h, int src_w, int dest_x, int dest_y, int dest_h, int dest_w) : Component(Component::Type::GRAPHICS)
+GraphicsComponent::GraphicsComponent(std::string* image_name, SDL_Rect* src, SDL_Rect* dest) : Component(Component::Type::GRAPHICS)
 {
 	Asset::image(image_name, &this->surface);
-	this->src_rect = new SDL_Rect;
-	this->src_rect->x = src_x;
-	this->src_rect->y = src_y;
-	this->src_rect->h = src_h;
-	this->src_rect->w = src_w;
-	this->dest_rect = new SDL_Rect;
-	this->dest_rect->x = dest_x;
-	this->dest_rect->y = dest_y;
-	this->dest_rect->h = dest_h;
-	this->dest_rect->w = dest_w;
-}
-
-GraphicsComponent::~GraphicsComponent()
-{
-	delete this->src_rect;
-	delete this->dest_rect;
+	this->src_rect = *src;
+	this->dest_rect = *dest;
 }
--- src/engine/GraphicsComponent.hpp
+++ src/engine/GraphicsComponent.hpp
@@ -9,13 +9,12 @@
 class GraphicsComponent : public Component
 {
     public:
-        GraphicsComponent(std::string* image_name, int src_x, int src_y, int src_h, int src_w, int dest_x, int dest_y, int dest_h, int dest_w);
-		virtual ~GraphicsComponent();
+        GraphicsComponent(std::string* image_name, SDL_Rect* src, SDL_Rect* dest);
 		virtual void update(int delta_time) {}
 
 		SDL_Surface* surface;
-		SDL_Rect* src_rect;
-		SDL_Rect* dest_rect;
+		SDL_Rect src_rect;
+		SDL_Rect dest_rect;
 		SDL_Texture* texture;
 		uint32_t layer;
 };
