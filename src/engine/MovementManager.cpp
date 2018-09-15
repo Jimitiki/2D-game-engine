e12cdf48284095a12aea49c5f3fce7e46ddfb603
Chris Johnson <cwjohnson42@gmail.com>
2018-09-15 10:31:08 -0700

    Fixed an incorrect condition

--- src/engine/MovementManager.cpp
+++ src/engine/MovementManager.cpp
@@ -7,7 +7,7 @@ void Movement::update(int delta_t, const std::vector<Entity*>* entities)
 {
 	for (Entity* entity : *entities)
 	{
-		if (entity->get_type_id() & (Component::Type::GRAPHICS | Component::Type::MOVEMENT))
+		if (entity->get_type_id() & Component::Type::MOVEMENT)
 		{
 			MovementComponent* component = (MovementComponent*) entity->get_component(Component::Type::MOVEMENT);
 			component->update(delta_t);
